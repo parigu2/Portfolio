19d0e1bf3173866f039980199a850ff994829d15
Minkyu Yang <parigu2@msn.com>
2018-11-17 09:45:00 -0600

    layout

---
+++ client/components/contact.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class Contact extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+   return (
+    <div>
+    Contact
+    </div>
+   )
+ }
+}
+
+// const mapStateToProps = state => {
+
+// }
+
+// const mapDispatchToProps = dispatch => {
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Main)
+
+export default Contact
