604f88bdd1d45885b415fce8d53da0ca30e82474
Minkyu Yang <parigu2@msn.com>
2018-11-17 11:09:35 -0600

    mapping project data

--- client/components/project.js
+++ client/components/project.js
@@ -7,11 +7,54 @@ import {
   Segment,
 } from 'semantic-ui-react'
 
+const projects = [{
+  name: 'Capstone - WIREHEAD',
+  detail: `First Instead of focusing on content creation and hard work, we have learned how to master the
+  art of doing nothing by providing massive amounts of whitespace and generic content that
+  can seem massive, monolithic and worth your attention.`,
+  image: 'favicon.png',
+  url: 'https://youtu.be/2Q_PyxAE3Mo'
+},{
+  name: 'Stackaton Proejct - 3D Game',
+  detail: `Second Instead of focusing on content creation and hard work, we have learned how to master the
+  art of doing nothing by providing massive amounts of whitespace and generic content that
+  can seem massive, monolithic and worth your attention.`,
+  image: 'favicon.png',
+  url: 'https://www.youtube.com/watch?v=OuLYWN4k3nk&feature=youtu.be'
+},{
+  name: 'Grace Shopper Project - Maskot',
+  detail: `Third Instead of focusing on content creation and hard work, we have learned how to master the
+  art of doing nothing by providing massive amounts of whitespace and generic content that
+  can seem massive, monolithic and worth your attention.`,
+  image: 'favicon.png',
+  url: 'https://www.youtube.com/watch?v=OuLYWN4k3nk&feature=youtu.be'
+}]
+
 const Project = () => (
   <Segment style={{ padding: '8em 0em' }} vertical>
     <Container text>
+      <Header as='h2' style={{ fontSize: '3em', margin: '1em 0em'}}>Project</Header>
+      {
+        projects.map(project=>{
+          return (
+          <div>
+          <Header as='h3' style={{ fontSize: '2em' }}>
+            {project.name}
+          </Header>
+          <p style={{ fontSize: '1.33em' }}>
+            {project.detail}
+          </p>
+          <Button as='a' size='small'>
+            Read More
+          </Button>
+          <Divider
+            style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          />
+        </div>)
+        })
+      }
       <Header as='h3' style={{ fontSize: '2em' }}>
-        Breaking The Grid, Grabs Your Attention
+        Capstone - WIREHEAD
       </Header>
       <p style={{ fontSize: '1.33em' }}>
         Instead of focusing on content creation and hard work, we have learned how to master the
